082c97571609ee5a27085a36ed9277229dd95724
abs2free <155795408+abs2free@users.noreply.github.com>
2025-11-06 15:05:19 +0800

    up

--- init.lua
+++ init.lua
@@ -908,6 +908,7 @@ require('lazy').setup({
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+        ['<C-l>'] = { 'show', 'show_documentation', 'hide_documentation' },
       },
 
       appearance = {
--- lua/custom/plugins/none-ls.lua
+++ lua/custom/plugins/none-ls.lua
@@ -3,12 +3,14 @@ return
 {
   'nvimtools/none-ls.nvim',
   opts = function(_, opts)
-    local nls = require 'null-ls'
+    local null_ls = require 'null-ls'
     opts.sources = opts.sources or {}
 
     vim.list_extend(opts.sources, {
+      null_ls.builtins.completion.spell,
+
       -- 静态检查 golangci-lint 为诊断工具
-      nls.builtins.diagnostics.golangci_lint,
+      null_ls.builtins.diagnostics.golangci_lint,
     })
   end,
 }
