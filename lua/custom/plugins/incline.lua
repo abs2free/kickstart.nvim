e57a8e95b731d5fad5215aa5dbbe11d8c7b7b8ef
abs2free <abs2free@gmail.com>
2026-02-28 11:28:53 +0800

    outline window show filename at the top right corner

--- lua/custom/plugins/incline.lua
+++ lua/custom/plugins/incline.lua
@@ -1,36 +1,60 @@
 return {
-  -- Adding a filename to the Top Right
-  {
-    'b0o/incline.nvim',
-    enabled = true,
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      local devicons = require 'nvim-web-devicons'
+  'b0o/incline.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons', 'stevearc/aerial.nvim' },
+  event = 'VeryLazy',
+  config = function()
+    local devicons = require 'nvim-web-devicons'
+    require('incline').setup {
+      hide = {
+        only_win = false,
+      },
+      window = {
+        margin = { vertical = 0, horizontal = 1 },
+        placement = { vertical = 'top', horizontal = 'right' },
+      },
+      render = function(props)
+        local bufname = vim.api.nvim_buf_get_name(props.buf)
+        local filename = vim.fn.fnamemodify(bufname, ':t')
+        if filename == '' then
+          filename = '[No Name]'
+        end
 
-      require('incline').setup {
-        hide = {
-          only_win = false,
-        },
-        render = function(props)
-          local bufname = vim.api.nvim_buf_get_name(props.buf)
-          local filename = vim.fn.fnamemodify(bufname, ':t')
-          if filename == '' then
-            filename = '[No Name]'
+        local ext = vim.fn.fnamemodify(bufname, ':e')
+        local icon, icon_color = devicons.get_icon(filename, ext, { default = true })
+        local modified = vim.bo[props.buf].modified
+
+        -- 🚀 修复核心：正确提取 Aerial 节点名称
+        local success, aerial = pcall(require, 'aerial')
+        local location = ''
+        if success then
+          local loc = aerial.get_location(true)
+
+          -- 检查返回值是否为 table 并且不为空
+          if type(loc) == 'table' and #loc > 0 then
+            local path_names = {}
+            -- 遍历 Aerial 返回的对象数组
+            for _, item in ipairs(loc) do
+              -- 提取每个节点对象中的 name 属性
+              if item.name then
+                table.insert(path_names, item.name)
+              end
+            end
+
+            -- 如果成功提取到了名称，就拼成面包屑
+            if #path_names > 0 then
+              location = ' ⟩ ' .. table.concat(path_names, ' ⟩ ')
+            end
           end
+        end
 
-          local ext = vim.fn.fnamemodify(bufname, ':e')
-          local icon, icon_color = devicons.get_icon(filename, ext, { default = true })
-
-          local modified = vim.bo[props.buf].modified
-
-          return {
-            { ' ', icon, ' ', guifg = icon_color },
-            { filename, gui = modified and 'bold' or 'none' },
-            modified and { ' [+]', guifg = '#ff9e64' } or '',
-            ' ',
-          }
-        end,
-      }
-    end,
-  },
+        return {
+          { ' ', icon, ' ', guifg = icon_color },
+          { filename, gui = modified and 'bold' or 'none' },
+          { location, guifg = '#888888' }, -- 灰色显示代码面包屑
+          modified and { ' [+]', guifg = '#ff9e64' } or '',
+          ' ',
+        }
+      end,
+    }
+  end,
 }
